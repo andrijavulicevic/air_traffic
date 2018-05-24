f24924afbca166015d5fff0f4f592a4fddd551db
andrija_vulicevic <andrija.vulicevic@netcast.rs>
2018-05-24 13:54:40 +0200

    Basic structure

---
+++ src/components/AirTraffic.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Main component</div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    this.$getLocation({
+      enableHighAccuracy: false,
+      timeout: Infinity,
+      maximumAge: 0
+    })
+      .then(coordinates => {
+        console.log(coordinates)
+      })
+  }
+}
+</script>
